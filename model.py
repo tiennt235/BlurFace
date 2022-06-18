3cd8023d80a595904f17628e6c433e9cc4fd303e
Tien Nguyen Tran <88872039+tiennt235@users.noreply.github.com>
2022-06-18 18:57:35 +0700

    Update outpath

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import cv2 as cv
 import os
+import pathlib
 
 def load_face_models():
 
@@ -49,7 +50,7 @@ def blur_face(video):
     size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
     out_fps = 30
     fourcc = cv.VideoWriter_fourcc(*'H264')
-    out_path = __file__ + 'result.mp4'
+    out_path = os.path.join(pathlib.Path(__file__).parent.resolve(), 'result.mp4')
     writer = cv.VideoWriter(out_path, fourcc, out_fps, size)
 
     while True:
