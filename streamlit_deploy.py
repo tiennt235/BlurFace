87961a27181a9611c7c5bfde878d1f85b05bb020
Tien Nguyen Tran <88872039+tiennt235@users.noreply.github.com>
2022-06-18 17:59:31 +0700

    Delete subprocess command

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import model
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
 video = st.file_uploader("Choose a file to upload")
 
 if video is not None:
