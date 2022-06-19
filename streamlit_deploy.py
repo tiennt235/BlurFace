c706aaf287da3490c1331f0ea7d76e3635de262c
tiennt235 <tiennt235@gmail.com>
2022-06-19 17:47:46 +0700

    add header

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -5,4 +5,4 @@ video = st.file_uploader("Choose a file to upload")
 
 if video is not None:
     result = model.blur_face(video.name)
-    st.video(result)
+    st.video(result, format="TIFF", add_headers=True)
